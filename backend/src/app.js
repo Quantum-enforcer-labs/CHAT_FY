5c964b63992387e7162ae9c62852461e1baf6cd8
Prosper Chitewe <Prosperchitewe691@gmail.com>
2025-09-24 00:50:01 +0200

    error fixing of path

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("*", (_, res) => {
-    res.sendFile(path.resolve(__dirname, "../../frontend/dist", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "../../frontend", "dist", "index.html")
+    );
   });
 }
 
